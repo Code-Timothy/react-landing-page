4020cafee85acbf7b3b729587ce5cea21b8451c9
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-16 23:27:03 +0100

    add globalStyles with alternative box model

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyles } from './core/globalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
