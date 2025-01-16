d24b31ba4b81dcdd2a0029a537a7aa4dc0b0bb98
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-16 23:52:29 +0100

    add font family to globalStyles

--- src/core/globalStyles.js
+++ src/core/globalStyles.js
@@ -3,9 +3,13 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
     html{
         box-sizing: border-box;
-    }
+    };
 
     *, ::before, ::after{
         box-sizing: inherit;
-    }
+    };
+
+    body{
+        font-family: "Raleway", sans-serif;
+    };
 `;
