52d378822da384d814deaafcc6bfc0c11d75b5c5
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-17 01:31:57 +0100

    add new colors to theme file

--- src/core/theme.js
+++ src/core/theme.js
@@ -1,5 +1,10 @@
 export const theme = {
     colors: {
         mineShaft: "#333333",
+        alabaster: "#F8F8F8",
+        emperor: "#555555",
+        white: "#FFFFFF",
+        malibu: "#5CA9FB",
+        cornflowerBlue: "#6372FF",
     },
 };
